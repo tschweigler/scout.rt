e907fc2cd71e8cfe0ecee26f47f126ca75f51f32
Jeremie Bresson <jbr@bsiag.com>
2013-02-18 11:19:15 +0100

    add tests from DynamicNlsTest

---
+++ org.eclipse.scout.rt.shared.test/src/org/eclipse/scout/rt/shared/TextsTest.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2013 BSI Business Systems Integration AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     BSI Business Systems Integration AG - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.scout.rt.shared;
+
+import java.util.Locale;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * JUnit tests for {@link ScoutTexts} and {@link TEXTS}
+ */
+public class TextsTest {
+
+  @Test
+  public void testGet() {
+    Assert.assertEquals("Info", TEXTS.get("Info"));
+    Assert.assertEquals("{undefined text anyKey}", TEXTS.get("anyKey"));
+  }
+
+  @Test
+  public void testGetTextMap() {
+    Map<String, String> textMap = ScoutTexts.getInstance().getTextMap(Locale.ENGLISH);
+    Assert.assertNotNull(textMap);
+    Assert.assertFalse(textMap.isEmpty());
+  }
+}

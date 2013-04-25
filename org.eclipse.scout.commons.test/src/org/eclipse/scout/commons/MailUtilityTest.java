09d03d84df9e079e8f69cc6b8e6c2139a050ccf5
Jeremie Bresson <jbr@bsiag.com>
2013-04-25 13:41:49 +0200

    Bug 00000
    * add fragment: org.eclipse.scout.rt.testing.shared.test
    * some corrections from review

--- org.eclipse.scout.commons.test/src/org/eclipse/scout/commons/MailUtilityTest.java
+++ org.eclipse.scout.commons.test/src/org/eclipse/scout/commons/MailUtilityTest.java
@@ -10,6 +10,9 @@
  ******************************************************************************/
 package org.eclipse.scout.commons;
 
+import java.io.ByteArrayOutputStream;
+
+import javax.activation.MailcapCommandMap;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
@@ -35,5 +38,29 @@ public class MailUtilityTest {
     Assert.assertNotNull(message);
     message = MailUtility.createMimeMessage(null, null, "Subject", "Body", null);
     Assert.assertNotNull(message);
+  }
+
+  /**
+   * encoding error caused by jax-ws
+   * <p>
+   * <b>Note: This test must be run alone in an isolated jre, otherwise it is not correct.</b>
+   */
+  //@Test
+  public void testJaxWsMimeMessageWithoutSender() throws Exception {
+    String mime = "text/plain; charset=utf-8";
+    //raw jre, everything fine
+    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    MailcapCommandMap.getDefaultCommandMap().createDataContentHandler("text/plain").writeTo("äöü", mime, bos);
+    Assert.assertArrayEquals(new byte[]{-61, -92, -61, -74, -61, -68}, bos.toByteArray());
+    //access jax-ws mime codec that adds the buggy mapping
+    Class.forName("com.sun.xml.internal.ws.encoding.MimeCodec");
+    bos = new ByteArrayOutputStream();
+    MailcapCommandMap.getDefaultCommandMap().createDataContentHandler("text/plain").writeTo("äöü", mime, bos);
+    Assert.assertArrayEquals(new byte[]{-28, -10, -4}, bos.toByteArray());
+    //activate MailUtility that fixes the bug
+    MailUtility.getContentTypeForExtension("html");
+    bos = new ByteArrayOutputStream();
+    MailcapCommandMap.getDefaultCommandMap().createDataContentHandler("text/plain").writeTo("äöü", mime, bos);
+    Assert.assertArrayEquals(new byte[]{-61, -92, -61, -74, -61, -68}, bos.toByteArray());
   }
 }

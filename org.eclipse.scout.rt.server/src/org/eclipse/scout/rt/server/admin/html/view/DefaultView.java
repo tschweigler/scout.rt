9f8aed7e0b692c0d3e18231454ca575aa23e17b6
Thomas Schweigler <thomas.schweigler@bsiag.com>
2013-09-17 08:36:45 +0200

    Serializable, transient

--- org.eclipse.scout.rt.server/src/org/eclipse/scout/rt/server/admin/html/view/DefaultView.java
+++ org.eclipse.scout.rt.server/src/org/eclipse/scout/rt/server/admin/html/view/DefaultView.java
@@ -4,18 +4,21 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
 package org.eclipse.scout.rt.server.admin.html.view;
 
+import java.io.Serializable;
+
 import org.eclipse.scout.rt.server.admin.html.AdminSession;
 import org.eclipse.scout.rt.server.admin.html.IView;
 import org.eclipse.scout.rt.server.admin.html.widget.table.HtmlComponent;
 
-public class DefaultView implements IView {
-  private AdminSession m_as;
+public class DefaultView implements IView, Serializable {
+  private static final long serialVersionUID = 1L;
+  private transient AdminSession m_as;
 
   public DefaultView(AdminSession as) {
     m_as = as;

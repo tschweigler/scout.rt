a64c30af2f28e2e4db7bf7191b1f45e1ae1aa040
Thomas Schweigler <thomas.schweigler@bsiag.com>
2013-10-16 13:17:41 +0200

    Test

--- org.eclipse.scout.rt.server/src/org/eclipse/scout/rt/server/AbstractServerSession.java
+++ org.eclipse.scout.rt.server/src/org/eclipse/scout/rt/server/AbstractServerSession.java
@@ -42,7 +42,7 @@ import org.osgi.framework.Bundle;
 
 public abstract class AbstractServerSession implements IServerSession, Serializable {
 
-  // Change for maven
+  // Change for maven 02
   private static final long serialVersionUID = 1L;
   private static final IScoutLogger LOG = ScoutLogManager.getLogger(AbstractServerSession.class);
 
